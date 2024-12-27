806b81fe3b37a99d21169e5d9b570ae5d5dc3894
ayushshukla-01 <ayushshukla8845@gmail.com>
2024-12-27 19:50:02 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+        const targetId = link.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
